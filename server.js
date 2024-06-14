1d1aa7c991196b0c22fd9a16b829f0a1a29454e0
Mayur Mandani <mmandani9@gmail.com>
2024-06-14 18:42:30 +0530

    Route changes are implemented

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const friends = [{
+        id: 0,
+        name: 'Albert Einstein'
+},{
+        id: 1,
+        name: 'Isaac Newton'
+}];
+
+app.use((req, res, next) => {
+        const start = Date.now();
+        next();
+        const delta = Date.now() - start;
+        console.log(`${req.method} ${req.url} ${delta}ms`);
+
+        // actions go here ...
+});
+
+app.get('/friends', (req, res) => {
+        res.json(friends);
+});
+
+app.get('/friends/:id', (req, res) => {
+        const id = Number(req.params.id);
+        const friend = friends[id];
+        if(friend) {
+                res.status(200).json(friend);
+        } else {
+             res.status(404).json({
+                error: "Friends does not exist"
+             });   
+        }
+})
+
+app.get('/messages', (req, res) => {
+        res.send('<ul><li>Hello Friends!!</li></ul>');
+});
+
+app.post('/messages', (req, res) => {
+        res.send('Updating messages...');
+});
+
+app.listen(PORT, () => {
+        console.log(`Listening on ${PORT}`);
+});
