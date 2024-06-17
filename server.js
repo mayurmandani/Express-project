039b50e907bf73ab7784ece2b3058e05dba5af68
Mayur Mandani <mmandani9@gmail.com>
2024-06-17 18:42:49 +0530

    Post Requests in Express changes are updated

--- server.js
+++ server.js
@@ -17,13 +17,28 @@ app.use((req, res, next) => {
         next();
         const delta = Date.now() - start;
         console.log(`${req.method} ${req.url} ${delta}ms`);
+});
 
-        // actions go here ...
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+   if(!req.body.name) {
+      return res.status(400).json({
+                error: 'Missing friend name'
+        });
+   }
+     const newFriend = {
+        name: req.body.name,
+        id: friends.length
+     };
+     friends.push(newFriend); 
+     res.json(newFriend);          
 });
 
 app.get('/friends', (req, res) => {
         res.json(friends);
 });
+
 
 app.get('/friends/:id', (req, res) => {
         const id = Number(req.params.id);
