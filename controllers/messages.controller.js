9fcd3e9a03ae54a117d01298b7c23c2cc5dea760
Mayur Mandani <mmandani9@gmail.com>
2024-06-18 16:00:05 +0530

    Layout, Template and static side code changes are updated

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,8 +1,12 @@
 const path = require('path');
 
 function getMessages(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
-  //  res.send('<ul><li>Hello Friends!!</li></ul>');
+    res.render('messages', {
+      title: 'Messages to my Friends!',
+      friend: 'Elon Musk',
+    });
+   // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
+
 }
 
 function postMessage(req,res) {
